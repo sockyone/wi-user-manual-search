c4436538b14a5b1eb9360f48dbf96a7c9bf910ff
NamPhan <sockyone16899@gmail.com>
2020-06-04 18:06:23 +0700

    hello

--- src/components/ContentSearch.vue
+++ src/components/ContentSearch.vue
@@ -4,7 +4,13 @@
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none" role="img" > <path fill-rule="evenodd" clip-rule="evenodd" d="M10.6002 12.0498C9.49758 12.8568 8.13777 13.3333 6.66667 13.3333C2.98477 13.3333 0 10.3486 0 6.66667C0 2.98477 2.98477 0 6.66667 0C10.3486 0 13.3333 2.98477 13.3333 6.66667C13.3333 8.15637 12.8447 9.53194 12.019 10.6419C12.0265 10.6489 12.0338 10.656 12.0411 10.6633L15.2935 13.9157C15.6841 14.3063 15.6841 14.9394 15.2935 15.33C14.903 15.7205 14.2699 15.7205 13.8793 15.33L10.6269 12.0775C10.6178 12.0684 10.6089 12.0592 10.6002 12.0498ZM11.3333 6.66667C11.3333 9.244 9.244 11.3333 6.66667 11.3333C4.08934 11.3333 2 9.244 2 6.66667C2 4.08934 4.08934 2 6.66667 2C9.244 2 11.3333 4.08934 11.3333 6.66667Z" /> </svg>
       <input type="search" placeholder="Search in user manual" autocomplete="off" v-model="searchText" style="background: #fff !important;" />
     </div>
-    <div class="resultContent">
+    <div v-if="loadingState">
+      Loading...
+    </div>
+    <div v-if="!loadingState" class="resultContent">
+      <div v-if="sentTextSearch.length > 0">
+        No results found for {{sentTextSearch}}
+      </div>
       <div v-for="data in results" :key="data._id" class="item-row">
         <highlight-element :data="data"></highlight-element>
       </div>
@@ -30,26 +36,49 @@ export default {
   data: function() {
     return {
       searchText: "",
-      results: []
+      results: [],
+      loadingState: false,
+      sentTextSearch: ""
     };
   },
   mounted: function() {
     let self = this;
     textSearchSubjectDebounced.subscribe(data => {
       //console.log('data:', data)
+
+      //start loading state
+      if (data.length === 0) {
+        self.results = []
+        return
+      }
+      self.sentTextSearch = data
+      self.loadingState = true
       axios
-        .post("http://112.137.129.229:30666/search", {
+        .post("https://manual.i2g.cloud/search", {
           textSearch: data
         })
         .then(rsFromSvr => {
-          console.log(rsFromSvr.data);
-          self.results = rsFromSvr.data;
+          //console.log(rsFromSvr.data);
+          if (rsFromSvr.data.length === 0) {
+            self.sentTextSearch = data;
+          } else {
+            self.sentTextSearch = "";
+          }
+          // if (rsFromSvr.data.length > 0) {
+          //   // sort
+          //   console.log(rsFromSvr.data)
+          // }
+          //console.log(rsFromSvr.data)
+          self.results = rsFromSvr.data.sort((a,b) => {
+            return a._source.chapter - b._source.chapter
+          });
+          self.loadingState = false
         });
     });
   },
   watch: {
     searchText(newText) {
-      textSearchSubject.next(newText);
+      textSearchSubject.next(newText)
     }
   }
 };
@@ -66,6 +95,7 @@ em {
   max-width: 600px;
   margin: 20px;
 }
+
 .background-search {
 
 }
